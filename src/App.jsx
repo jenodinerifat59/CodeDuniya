cdcc42fb86ddf67748fca6b567a5507490e8629d
jenodinerifat59 <mdjenodineislamrifat@gmail.com>
2025-11-11 17:38:45 +0600

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ListItems from './components/ListItems.jsx'
+// import ListItems from './components/ListItems.jsx'
 import NavItem from './layouts/navItem.jsx'
 import Hero from './layouts/Hero.jsx'
 import Catagory from './layouts/Catagory.jsx'
